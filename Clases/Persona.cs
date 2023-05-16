f5188070097bb8c19e56cd8bd274b38fe22c7da5
robertoofb <robertojunio2004@gmail.com>
2023-05-16 12:17:41 -0600

    subiendo el repo

---
+++ Clases/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objetos.Clases
+{
+    //clases es una plantilla
+    //que contiene objetos y metodos
+    public class Persona
+    {
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public double Salario { get; set; }
+
+
+        public void ImprimirDatos()
+        {
+            Console.WriteLine("Tu nombre es: " + this.Nombre);
+            Console.WriteLine("Tu apellido es: " + this.Apellido);
+            Console.WriteLine("Tu edad es: " + this.Edad);
+            Console.WriteLine("Tu Salario es: " + this.Salario);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Objetos.Clases;
+using System;
+
+namespace Objetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola guapo");
+            //instanciar: crear un nuevo objeto REAL
+            Persona persona = new Persona();
+            persona.Nombre = "Roberto";
+            persona.Apellido = "Fierro";
+            persona.Edad = 18;
+            persona.Salario = 15000.00;
+
+            persona.ImprimirDatos();
+
+            Console.WriteLine("-----------------------");
+
+            Persona persona2 = new Persona();
+            persona2.Nombre = "Axel";
+            persona2.Apellido = "Ramón";
+            persona2.Edad = 24;
+            persona2.Salario = 22000.00;
+
+            persona2.ImprimirDatos();
+
+        }
+    }
+}
